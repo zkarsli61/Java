97c07252498d2f8796f523b43969bba50ad57528
zkarsli <zkarsli61@gmail.com>
2022-01-14 12:20:55 +0300

    praciting abstract method

--- src/main/java/day26Inheritance/RunnerClass.java
+++ src/main/java/day26Inheritance/RunnerClass.java
@@ -2,6 +2,7 @@ package day26Inheritance;
 
 public class RunnerClass extends C {
     public static void main(String[] args) {
+        //C obj1 = new C(); // 'C' is abstract; cannot be instantiated
         C obj = new RunnerClass();
         obj.m();
     }
